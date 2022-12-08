edc0270a1835a0652111e63834b16ba5915df8cf
chaning49 <ggammancj@gmail.com>
2022-12-08 14:00:27 +0900

    first commit

---
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
